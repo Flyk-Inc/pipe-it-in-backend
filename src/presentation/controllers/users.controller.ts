d119bdab7a9843a403c16f5697faf424bf81742a
lyeslimani <lyes.slimani.dev@gmail.com>
2024-05-08 23:51:24 -0400

    fix: profile route data

--- src/presentation/controllers/users.controller.ts
+++ src/presentation/controllers/users.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../../infrastructure/auth/guards/jwt-auth.guard';
 import { SignedInRequest } from '../../infrastructure/auth/strategies/jwt.strategy';
+import { UsersService } from '../../domain/users/service/users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req: SignedInRequest) {
-    return req.user;
+    return this.usersService.getUserByEmail(req.user.email);
   }
 }
