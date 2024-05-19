e853796d107ef88f1f21e7af7eb7805167e67bac
flammefolie <fguillemont@myges.fr>
2024-05-20 01:30:46 +0200

    chore: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { configDotenv } from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 configDotenv();
 
@@ -10,6 +11,15 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Pipe-It-In API')
+    .setDescription('API for the Pipe-It-In backend')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   console.log(`Application is running on port:${port}`);
 }
